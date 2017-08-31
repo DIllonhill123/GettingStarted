0c8f44e15641501aa1cfb6134b62b5fd4285b10d
Dillon Hill <dillonhill9@gmail.com>
2017-08-31 10:28:11 -0400

    Favorite Food
    Food project

---
+++ FavoriteFood/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorte food?");
+            String input = Console.ReadLine();
+            if (input == "Burgers")
+            {
+                Console.WriteLine("10/10 Best Food");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Bad Food");
+            }
+        }
+    }
+}
