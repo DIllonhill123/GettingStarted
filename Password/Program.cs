10fd93f0cb66ae0f2d1b9db5f8a416118dd30122
DIllonhill123 <dillonhill9@gmail.com>
2017-09-06 11:28:37 -0400

    Password
    Password Proj Started

---
+++ Password/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int a = 0; a < 3; a++)
+            {
+                Console.WriteLine("Enter Username:");
+                String user = Console.ReadLine();
+                Console.WriteLine("Enter Password:");
+                String pass = Console.ReadLine();
+
+                if (user == "root" && pass == "Letmein")
+                {
+                    Console.WriteLine("Welcome!");
+                    a = 3;
+                }
+            }
+        
+        }
+    }
+}
