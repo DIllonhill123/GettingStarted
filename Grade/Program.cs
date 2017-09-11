5de71d291fa8e5da6e3f19fe30d4694469758557
DIllonhill123 <dillonhill9@gmail.com>
2017-09-11 13:46:55 -0400

    Grade Match
    Started and Finished Project

---
+++ Grade/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grade Matchamajigger Initialized");
+            Console.WriteLine("Input the Grade:");
+            String grade = Console.ReadLine();
+
+            if(grade == "e")
+            {
+                Console.WriteLine("You have chosen: Excellent");
+            }
+            if (grade == "v")
+            {
+                Console.WriteLine("You have chosen: Very Good");
+            }
+            if (grade == "g")
+            {
+                Console.WriteLine("You have chosen: Good");
+            }
+            if (grade == "a")
+            {
+                Console.WriteLine("You have chosen: Average");
+            }
+            if (grade == "f")
+            {
+                Console.WriteLine("You have chosen: Fail");
+            }
+            else
+            {
+                Console.WriteLine("Not a real Grade");
+            }
+
+        }
+    }
+}
