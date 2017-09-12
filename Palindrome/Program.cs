4992c5d66737d6e35f4354e2235f520bf4ea7dc7
DIllonhill123 <dillonhill9@gmail.com>
2017-09-12 10:53:26 -0400

    Palindrome
    Started and Finised Proj

---
+++ Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindome Checker");
+            while (1 > 0)
+            {
+                Console.WriteLine("Input a word:");
+                string word = Console.ReadLine();
+
+                Char[] wordArr = word.ToCharArray();
+                Array.Reverse(wordArr);
+                string drow = new string(wordArr);
+
+                if (word == drow)
+                {
+                    Console.WriteLine(word + " is a palindrome");
+                }
+                else
+                {
+                    Console.WriteLine(word + " is not a palindrome");
+                }
+            }
+        }
+    }
+}
