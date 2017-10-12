6aadf56c36ea7978b9120328c2026b566f2b0f67
DIllonhill123 <dillonhill9@gmail.com>
2017-10-12 14:19:59 -0400

    whatever

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] Arr = new String[100];
+            for(int i = 0; i < Arr.Length; i++)
+            {
+                Arr[i] = i + ". ";
+                if(i % 3 == 0)
+                {
+                    Arr[i] += "Fizz";
+                }
+                if(i % 5 == 0)
+                {
+                    Arr[i] += "Buzz";
+                }
+            }
+            for(int i = 0; i < Arr.Length; i++)
+            {
+                Console.WriteLine(Arr[i]);
+            }
+            
+        }
+    }
+}
---
+++ GettingStarted/UniqueDigits/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] Arr = new int[50];
+            Random rnd = new Random();
+            int[] Arr2 = new int[50];
+            bool dupe = false;
+
+            for(int i = 0; i < Arr.Length; i++)
+            {
+                Arr[i] = rnd.Next(1, 50);
+                
+            }
+            Array.Sort(Arr);
+            for(int i = 0; i < Arr.Length; i++)
+            {
+                for(int j = i+1; j < Arr.Length; j++)
+                {
+                    if(Arr[i] == Arr[j])
+                    {
+                        dupe = true;
+                        
+                    }
+                    break;
+
+                }
+                if(dupe == false)
+                {
+                    Arr2[i] = Arr[i];
+                }
+                dupe = false;
+            }
+            for(int i = 0; i < Arr2.Length; i++)
+            {
+                Console.WriteLine(Arr2[i]);
+            }
+
+        }
+    }
+}
